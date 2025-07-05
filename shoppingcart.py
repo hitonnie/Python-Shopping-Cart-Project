fbe646f63eb6f4e5ef96ec881281991ff961e45c
hitonnie <161940723+hitonnie@users.noreply.github.com>
2025-07-06 00:57:51 +0200

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,6 +1,5 @@
-# Create a shopping cart programme that will continuously ask the user for a food product and the price of that product
-# Have an exit clause if the user wishes to stop adding more things to their cart
-# At the end show the food items and the total cost to the user
+# A shopping cart programme that will ask the user for a food and the price of that product
+# At the end it will show the food items and the total cost to the user
 
 foods = []
 prices = []
